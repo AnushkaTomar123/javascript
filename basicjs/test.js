61cb567852841af43fd6428f0b866365e3386326
Anushka Tomar <147533075+AnushkaTomar123@users.noreply.github.com>
2024-03-02 05:47:40 +0000

    container config done

--- basicjs/test.js
+++ basicjs/test.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+"use strict"; // treat all js code as newer version
+let studentName="tony";
+const studentId=100236;
+let studentAge=20;
+let studentRollno=35;
+console.table([studentName,studentId,studentAge])
+console.log(studentRollno)
+console.log(typeof studentName)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
